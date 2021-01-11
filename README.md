7fce1e5c89f8d51e68533466281f373e4824a1ce
waman <waman10da@gmail.com>
2021-01-11 11:23:04 +0900

    Add README.md (trial)

---
+++ README.md
@@ -0,0 +1,13 @@
+##概要
+
+```
+  x₁ = r sinθ₁ sinφ₁
+  x₂ = r sinθ₁ cosφ₁
+  x₃ = r cosθ₁ sinφ₂
+  x₄ = r cosθ₁ cosφ₂
+```
+
+```
+  dV = r⁴sin²θ₁cosθ₁drdθ₁dφ₁dφ₂
+     = (1/15)d(r⁵)d(sin³θ₁)dφ₁dφ₂
+```
--- src/main/java/org/waman/washiki/RandomGenerator.java
+++ src/main/java/org/waman/washiki/RandomGenerator.java
@@ -5,11 +5,14 @@ public interface RandomGenerator{
 
     /**
      * Return a random number between 0 and 1.
+     * @return a random number between 0 and 1
      */
     double nextDouble();
 
     /**
-     * Return a random number between 0 and <code>width</code>.
+     * Return a random number between 0 and <code>width</code>1.
+     * @param width -
+     * @return a random number between 0 and <code>width</code>
      */
     default double nextDouble(double width){
         return width * nextDouble();
@@ -17,16 +20,11 @@ public interface RandomGenerator{
 
     /**
      * Return a random number between <code>min</code> and <code>min+width</code>.
+     * @param min -
+     * @param width -
+     * @return a random number between <code>min</code> and <code>min+width</code>
      */
     default double nextDouble(double min, double width){
         return min + width * nextDouble();
-    }
-
-    /**
-     * Return a random number between <code>min</code> and <code>max</code>.
-     * This method is equivalent to <code>nextDouble(min, max - min)</code>.
-     */
-    default double nextDoubleIn(double min, double max){
-        return nextDouble(min, max-min);
     }
 }
